65e9c026daef884e9442ead8f308c07baea9fa88
Tony Arcieri <bascule@gmail.com>
2014-04-13 16:59:39 -0700

    Fix Rubocop nags

--- spec/ordo/message_spec.rb
+++ spec/ordo/message_spec.rb
@@ -1,10 +1,16 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe ORDO::Message do
+  let(:example_cipher)   { 'ordo.symmetric-encryption:///xsalsa20poly1305' }
+  let(:example_encoding) { 'binary' }
+  let(:example_message)  { 'Some things are better left unread' }
+
   it 'parses messages' do
     message = described_class.parse test_vector('example_message.txt')
-    expect(message['Cipher']).to eq 'ordo.symmetric-encryption:///xsalsa20poly1305'
-    expect(message['Content-Transfer-Encoding']).to eq 'binary'
-    expect(message.body).to eq 'Some things are better left unread'
+    expect(message['Cipher']).to eq example_cipher
+    expect(message['Content-Transfer-Encoding']).to eq example_encoding
+    expect(message.body).to eq example_message
   end
 end
